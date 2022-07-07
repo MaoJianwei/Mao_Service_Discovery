c88a2378a2b367d40ab3ae9e8cfee9b7ec7fe312
Jianwei Mao <maojianwei2012@126.com>
2022-07-08 00:57:01 +0800

    test pass: new level HOT_DEBUG
    Signed-off-by: Jianwei Mao <maojianwei2012@126.com>

--- util/mao_log_test.go
+++ util/mao_log_test.go
@@ -6,8 +6,68 @@ import (
 )
 
 func TestInitMaoLog(t *testing.T) {
+	InitMaoLog(DEBUG)
+	MaoLogM(DEBUG, "Test", "out %d", 666)
+	MaoLogM(HOT_DEBUG, "Test", "out %d", 666)
+	MaoLogM(INFO, "Test", "out %d", 666)
+	MaoLogM(WARN, "Test", "out %d", 666)
+	MaoLogM(ERROR, "Test", "out %d", 666)
+	MaoLogM(SILENT, "Test", "out %d", 666)
+
+	log.Println("==============================================")
+
+	InitMaoLog(HOT_DEBUG)
+	MaoLogM(DEBUG, "Test", "out %d", 666)
+	MaoLogM(HOT_DEBUG, "Test", "out %d", 666)
+	MaoLogM(INFO, "Test", "out %d", 666)
+	MaoLogM(WARN, "Test", "out %d", 666)
+	MaoLogM(ERROR, "Test", "out %d", 666)
+	MaoLogM(SILENT, "Test", "out %d", 666)
+
+	log.Println("==============================================")
+
+	InitMaoLog(INFO)
+	MaoLogM(DEBUG, "Test", "out %d", 666)
+	MaoLogM(HOT_DEBUG, "Test", "out %d", 666)
+	MaoLogM(INFO, "Test", "out %d", 666)
+	MaoLogM(WARN, "Test", "out %d", 666)
+	MaoLogM(ERROR, "Test", "out %d", 666)
+	MaoLogM(SILENT, "Test", "out %d", 666)
+
+	log.Println("==============================================")
+
+	InitMaoLog(WARN)
+	MaoLogM(DEBUG, "Test", "out %d", 666)
+	MaoLogM(HOT_DEBUG, "Test", "out %d", 666)
+	MaoLogM(INFO, "Test", "out %d", 666)
+	MaoLogM(WARN, "Test", "out %d", 666)
+	MaoLogM(ERROR, "Test", "out %d", 666)
+	MaoLogM(SILENT, "Test", "out %d", 666)
+
+	log.Println("==============================================")
+
+	InitMaoLog(ERROR)
+	MaoLogM(DEBUG, "Test", "out %d", 666)
+	MaoLogM(HOT_DEBUG, "Test", "out %d", 666)
+	MaoLogM(INFO, "Test", "out %d", 666)
+	MaoLogM(WARN, "Test", "out %d", 666)
+	MaoLogM(ERROR, "Test", "out %d", 666)
+	MaoLogM(SILENT, "Test", "out %d", 666)
+
+	log.Println("==============================================")
+
+	InitMaoLog(SILENT)
+	MaoLogM(DEBUG, "Test", "out %d", 666)
+	MaoLogM(HOT_DEBUG, "Test", "out %d", 666)
+	MaoLogM(INFO, "Test", "out %d", 666)
+	MaoLogM(WARN, "Test", "out %d", 666)
+	MaoLogM(ERROR, "Test", "out %d", 666)
+	MaoLogM(SILENT, "Test", "out %d", 666)
+
+	log.Println(SILENT > DEBUG)
 	log.Println(ERROR > DEBUG)
 	log.Println(WARN > DEBUG)
 	log.Println(INFO > DEBUG)
+	log.Println(HOT_DEBUG > DEBUG)
 	log.Println(DEBUG > DEBUG)
 }
