14126d7b20d0a4f84036cc49acce3b671b58b5e5
Jianwei Mao <maojianwei2012@126.com>
2023-11-10 01:58:12 +0800

    add script to analyze dependency relationships among modules.
    Signed-off-by: Jianwei Mao <maojianwei2012@126.com>

---
+++ analyze_dependency_map.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+DEP_MAP_FILE="/tmp/mao_deps_map.csv"
+
+allDepsDup=""
+for gogo in `find | grep "\.go"`
+do
+    moduleName=`cat ${gogo} | grep -m1 "MODULE_NAME =" | awk -F '"' '{print $2}'`
+    for deps in `cat ${gogo} | grep "MaoCommon" | grep "Service" | grep -v "//" | awk -F 'Get' '{print $2}' | sed 's/()//' | grep -vE ^$`
+    do
+        thisDep="$moduleName=>$deps"
+        allDepsDup="$allDepsDup\n$thisDep"
+    done
+done
+
+echo "=================================================="
+echo -e $allDepsDup | grep -vE ^$ | wc -l
+echo -e $allDepsDup | grep -vE ^$ | sort -u | wc -l
+echo "**************************************************"
+
+allDepsUnique=`echo -e $allDepsDup | grep -vE ^$ | sort -u`
+allDepsDup=`echo -e $allDepsDup | grep -vE ^$`
+
+rm $DEP_MAP_FILE
+
+for uni in $allDepsUnique
+do
+    count=0
+    for dup in $allDepsDup
+    do
+        if [ $uni = $dup ]
+        then
+            let count++
+            # count=$((count+1))
+        fi
+    done
+    echo "$uni=>$count"
+    echo "$uni=>$count" | sed 's/=>/,/g' >> $DEP_MAP_FILE
+done
+
+echo "=================================================="
